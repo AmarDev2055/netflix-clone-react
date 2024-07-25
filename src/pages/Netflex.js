6abafb9abd417359d6467a4aa20775924f3a6abd
Amar Dev <devamar029@gmail.com>
2024-07-25 16:58:48 +0545

    Netflix Updated

--- src/pages/Netflex.js
+++ src/pages/Netflex.js
@@ -1,44 +1,142 @@
-import React, { useState } from 'react'
-import TopNav from '../components/TopNav';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-const Netflex = () => {
+import TopNav from "../components/TopNav";
+import Card from "../components/Card";
+import { fetchMovies, getGenres } from "../store";
+import SliderContainer from "../components/SliderContainer";
+
+const Netflix = () => {
   const [isScrolled, setIsScrolled] = useState(false);
 
-  window.onscroll = () => {
-    setIsScrolled(window.pageYOffset === 0 ? false : true)
-    return () => (window.onscroll = null)
-  }
-  console.log(isScrolled);
-  return (
-    <div className='hero'>
-      <TopNav isScrolled={isScrolled} />
-      <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg'
-       alt='Internet not Connected'
-       style={imageStyle}
-       />
-      <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg'
-       alt='Internet not Connected'
-       style={imageStyle}
-       />
-      <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg'
-       alt='Internet not Connected'
-       style={imageStyle}
-       />
-      <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg'
-       alt='Internet not Connected'
-       style={imageStyle}
-       />
-      <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg'
-       alt='Internet not Connected'
-       style={imageStyle}
-       />
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-const imageStyle = {
-  width: '100vw',
+  const movies = useSelector((state)=> state.netflix.movies)
+  const generesLoaded = useSelector((state)=>state.netflix.generesLoaded)
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getGenres());
+  }, []);
+
+  useEffect(() => {
+        if(generesLoaded){
+          dispatch(fetchMovies({type: "all"}))
+        }
+  });
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
+  // console.log(movies)
+
+  return (
+    <HeroContainer>
+      <div className="hero">
+        <TopNav isScrolled={isScrolled} />
+        <img
+          className="background-image"
+          src="https://img.sflix.to/xxrz/1200x600/224/5e/66/5e66f19ae1b2631fa9fa51561874a9a8/5e66f19ae1b2631fa9fa51561874a9a8.jpg"
+          alt="hero image"
+        />
+        <div className="container">
+          <div className="title">
+            <h1>Kingdom of the Planet of the Apes</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
+              tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrum exercitationem ullam corporis suscipit
+              laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute
+              iure reprehenderit in voluptate velit esse cillum dolore eu
+            </p>
+          </div>
+          <div className="buttons">
+            <button onClick={() => navigate("/player")} className="playBtn">
+              Play
+            </button>
+            <button className="moreBtn">More</button>
+          </div>
+        </div>
+      </div>
+     <SliderContainer movies={movies}/>
+    </HeroContainer>
+  );
 };
 
-export default Netflex
+const HeroContainer = styled.div`
+  .hero {
+    position: relative;
+    .background-image {
+      filter: brightness(40%);
+    }
+    img {
+      height: 100vh;
+      width: 100%;
+    }
+    .container {
+      position: absolute;
+      bottom: 1rem;
+      .title {
+        h1 {
+          margin-left: 5rem;
+          margin-top: 5rem;
+          text-transform: uppercase;
+          font-size: 50px;
+          background:white;
+          -webkit-background-clip: text;
+          -webkit-text-fill-color: transparent;
+        }
+        p {
+          margin-bottom: -50px;
+          width: 640px;
+          margin-left: 5rem;
+          font-family: "lexend Deca", sans-serif;
+          color: white;
+        }
+      }
+      .buttons {
+        display: flex;
+        margin: 5rem;
+        gap: 2rem;
+      }
+
+      .playBtn {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: red;
+        border-radius: 1rem;
+        font-size: 1.4rem;
+        gap: 1rem;
+        padding: 0.9rem;
+        padding-left: 2rem;
+        padding-right: 2.4rem;
+        border: none;
+        cursor: pointer;
+      }
+      .moreBtn {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: white;
+        background-color: black;
+        border-radius: 1rem;
+        font-size: 1.4rem;
+        gap: 1rem;
+        padding: 0.9rem;
+        padding-left: 2rem;
+        padding-right: 2.4rem;
+        border: 0.1rem solid white;
+        cursor: pointer;
+      }
+    }
+  }
+`;
+
+export default Netflix;
---
+++ src/utils/constant.js
@@ -0,0 +1,2 @@
+export const MY_API_KEY = "ead8b57f10b3fcdd938779e0bb455e7d";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
